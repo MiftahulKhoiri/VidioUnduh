e28412e32d54d2bb47870ea0fda8b75af7bd49db
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-06-15 12:58:46 +0800

    Update VidioUnduh.py

--- VidioUnduh.py
+++ VidioUnduh.py
@@ -1,1 +1,19 @@
-program awal
+import sys
+import os
+
+# Pastikan path folder modul dikenali Python
+sys.path.append(os.path.join(os.path.dirname(__file__), 'modul'))
+
+import update
+
+def main():
+    # Jalankan proses update terlebih dahulu
+    update.proses_update()
+    
+    # Setelah update selesai, jalankan main.py
+    # Import main.py sebagai modul
+    import main
+    main.main()  # Pastikan di main.py ada fungsi main()
+
+if __name__ == "__main__":
+    main()
