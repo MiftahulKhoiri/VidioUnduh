c2098d5572ca88b165ffde1a34177e1ea11d524c
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-06-16 18:36:47 +0800

    Update logo.py

--- modul/logo.py
+++ modul/logo.py
@@ -1,6 +1,5 @@
 # logo.py
 def tampilkan_logo():
-    # ANSI escape codes untuk warna
     CYAN = '\033[96m'
     BLUE = '\033[94m'
     GREEN = '\033[92m'
@@ -8,12 +7,9 @@ def tampilkan_logo():
     RESET = '\033[0m'
 
     logo = f"""
-{CYAN}██    ██ ██   ██ ██████  ██ ██████   ██████  ██    ██ █    ██ ██████  ██   ██
-{BLUE} ██  ██  ██   ██ ██   ██ ██ ██   ██ ██    ██ ██    ██ ██  ██  ██   ██  ██ ██ 
-{GREEN}  ████   ███████ ██████  ██ ██████  ██    ██ ██    ██ ██ ██   ██████    ███  
-{YELLOW}   ██    ██   ██ ██      ██ ██      ██    ██ ██    ██ ██  ██  ██         ██  
-{CYAN}   ██    ██   ██ ██      ██ ██       ██████   ██████  ██   ██ ██         ██   
-
-           {YELLOW}VidioUnduh - Download Video Tools{RESET}
+{CYAN}╔═══════════════════════╗
+{BLUE}║ {GREEN}Vidio{YELLOW}Unduh{BLUE}          ║
+{BLUE}║ {CYAN} Vidio Download Tools {BLUE}║
+{CYAN}╚═══════════════════════╝{RESET}
     """
     print(logo)
