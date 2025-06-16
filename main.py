a80a25616bb6dfc967c50bac9e575a52d9c0b1be
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-06-16 18:10:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+import time  # Untuk menambah jeda waktu
+
+def hapus_layar():
+    """Membersihkan layar terminal di semua OS."""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
 def main():
  print ("main.py")
 
