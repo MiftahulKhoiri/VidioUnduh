f0dbd3d9842693527b6889d6b76915edea3ca2fa
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-06-16 18:26:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 import time  # Untuk menambah jeda waktu
+from modul.logo import tampilkan_logo  # Import dari folder modul
 
 def hapus_layar():
     """Membersihkan layar terminal di semua OS."""
     os.system('cls' if os.name == 'nt' else 'clear')
 
-
-
 def main():
- print ("main.py")
+    hapus_layar()
+    tampilkan_logo()  # Memanggil fungsi logo dari modul/logo.py
+    print("main.py")
 
 if __name__ == "__main__":
     main()
