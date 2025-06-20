c4b4511240f4661064b5e9175738d1a547c5af62
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-06-20 19:29:31 +0800

    Update logo.py

--- modul/logo.py
+++ modul/logo.py
@@ -1,9 +1,10 @@
 from colorama import Fore, Style
-    CYAN = '\033[96m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RESET = '\033[0m'
+
+CYAN = '\033[96m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RESET = '\033[0m'
 
 def tampilkan_logo():
    
